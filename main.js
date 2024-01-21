12661e9d5479133593b543f6b6b32d0a3dd61c69
RadiooGaga <mariahkatt@gmail.com>
2024-01-21 20:30:03 +0100

    comit

--- main.js
+++ main.js
@@ -7,10 +7,8 @@ h1.textContent = "MARIA RIVAS";
 const h2 = document.createElement("h2");
 h2.textContent = "FRONTEND DEVELOPER";
 
-const introContentSection = document.querySelector("#introContentPage");
-const skillsDiv = document.createElement("div");
-skillsDiv.className = "skills";
-const menu = document.createElement("div");
+const contentPage = document.querySelector("#introContentPage");
+const menu = document.querySelector(".menuDiv");
 menu.className = "menuDiv";
 const leftSide = document.createElement("div");
 leftSide.className = "menuLeft";
@@ -20,9 +18,11 @@ const imgAvatar = document.createElement("img");
 imgAvatar.src = "./assets/icons/favicon.png";
 const rightSide = document.createElement("div");
 rightSide.className = "menuRight";
-const aboutMeDiv = document.createElement("div");
-aboutMeDiv.className = "aboutMe";
 
+const aboutMeDiv = document.querySelector(".aboutMeDiv");
+const aboutMeButton = document.createElement("button");
+aboutMeButton.className = "aboutMeButton";
+aboutMeButton.textContent = "ABOUT ME";
 
 const footerDivHTML = document.querySelector(".footerDiv");
 const footerSpan = document.createElement("span");
@@ -30,15 +30,12 @@ footerSpan.textContent = "© Created by Maria Rivas";
 
 
 
-
-
 headerDivHTML.appendChild(h1);
 headerDivHTML.appendChild(h2);
-introContentSection.appendChild(skillsDiv);
-introContentSection.appendChild(menu);
 menu.appendChild(leftSide);
 menu.appendChild(avatarDiv);
 avatarDiv.appendChild(imgAvatar);
 menu.appendChild(rightSide);
-introContentSection.appendChild(aboutMeDiv);
+contentPage.appendChild(aboutMeDiv);
+aboutMeDiv.appendChild(aboutMeButton);
 footerDivHTML.appendChild(footerSpan);
